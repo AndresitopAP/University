d6e5aa86d9a0906e0adc78a4cf63dfd754bfb824
AndresitopAP <andrespitia29@gmail.com>
2023-11-19 22:12:25 -0400

    final(creo)

--- Web (Html, CSS, JS)/Actividad #7 (porfolio)/script/index.js
+++ Web (Html, CSS, JS)/Actividad #7 (porfolio)/script/index.js
@@ -4,3 +4,31 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+var data = [];
+
+function sendData(){
+  const name = document.querySelector("#name").value;
+  const lastname = document.querySelector("#lastname").value;
+  const id = document.querySelector("#id").value;
+  const number = document.querySelector("#number").value;
+  const email = document.querySelector("#mail").value;
+  const message = document.querySelector("#message").value;
+
+  if(name == "" || lastname == "" || id == "" || number == "" || email == "" || message == ""){
+    alert("Por favor llene todos los campos");
+    return false;
+  }
+
+  data.push({
+      name: name,
+      lastname: lastname,
+      id: id,
+      number: number,
+      email: email,
+      message: message
+  });
+
+  console.log(data);
+}
